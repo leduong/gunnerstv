c6f7d5f54297824ce6b08e40056578232668c128
Sylvain Giuliani <sgiuliani@gmail.com>
2012-12-30 16:49:21 +0000

    test heroku

---
+++ app.py
@@ -0,0 +1,56 @@
+from flask import Flask, request, render_template
+import os
+from flask.ext.sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///gunnerstv.db'
+db = SQLAlchemy(app)
+
+
+class Fixture(db.Model):
+    id = db.Column(db.Integer)
+    opponenent = db.Column(db.String(80))
+    home = db.Column(db.Boolean(),default=True)
+    channel = db.Column(db.String(120))
+    channel_us = db.Column(db.String(120))
+    date = db.Column(db.DateTime,primary_key=True)
+    competition = db.Column(db.String(140))
+
+    def __init__(self, opponenent, home, channel, channel_us, date, competition):
+        self.opponenent = opponenent
+        self.home = home
+        self.channel = channel
+        self.channel_us = channel_us
+        self.date = date
+        self.competition = competition
+
+    def __repr__(self):
+        return '<fixure against %r in %r at home? %r on %r channel %r>' % (self.opponenent,self.competition,self.home,self.date,self.channel)
+
+
+
+@app.route('/', methods=['GET'])
+def index():
+	now = datetime.now()
+	today = False
+	fixtures = Fixture.query.all()
+	dates = []
+	for f in fixtures:
+		if f.date > now:
+			dates.append(f.date)
+	fixture_date = min(dates,key=lambda date : abs(now-date))
+	if fixture_date.date() == now.date():
+		today = True
+	remaining_day = (fixture_date - now).days
+	fixture = Fixture.query.filter_by(date=fixture_date).first()
+
+	return render_template('index.html',fixture=fixture,today=today,remaining_day=remaining_day)
+
+
+
+if __name__ == '__main__':
+	# Bind to PORT if defined, otherwise default to 5000.
+	port = int(os.environ.get('PORT', 5000))
+	app.run(host='0.0.0.0', port=port, debug=True)
---
+++ helpers.py
@@ -0,0 +1,89 @@
+from bs4 import BeautifulSoup
+import requests
+from app import db
+from app import Fixture
+from datetime import datetime,timedelta
+import json
+import pytz
+
+"""
+http://www.premierleague.com/content/premierleague-ajax/broadcastschedule.tv.ajax/season:2012-2013/country:US/clubId:1006/rangeType:dateSeason
+"""
+
+#Scrap fixtures from premierleague.com
+def get_us_data():
+	url = "http://www.premierleague.com/content/premierleague-ajax/broadcastschedule.tv.ajax/season:2012-2013/country:US/clubId:1006/rangeType:dateSeason"
+	r = requests.get(url)
+	data = r.json
+	uktz = pytz.timezone('GMT')
+	ustz = pytz.timezone('US/Eastern')
+	for date in data['dateList']:
+		for broadcast in date['broadcastList']:
+			channel_us = broadcast['channel']
+			date = broadcast['date'] + " " + broadcast['time']
+			date = datetime.strptime(date,'%A %d %B %Y %H:%M')
+			print date
+			if Fixture.query.filter_by(date=date).first():
+				entry = Fixture.query.filter_by(date=date).first()
+				entry.channel_us = channel_us
+				db.session.commit()
+			else:
+				print "I should add this date for US viewer"
+
+
+#scrap date on gunners
+def scrap_fixture_time():
+	#Reset db for testing
+	db.drop_all()
+	db.create_all()
+	#get date variables
+	uktz = pytz.timezone('GMT')
+	current_date = datetime.now(uktz)
+	current_month = current_date.strftime('%B')
+	next_month_date = current_date + timedelta(days=30)
+	next_month = next_month_date.strftime('%B')
+	#Scrapping arsenal.com
+	url = "http://www.arsenal.com/fixtures/fixtures-reports"
+	response = requests.get(url)
+	html = response.text
+	soup = BeautifulSoup(html,'lxml')
+	table_month = soup.find('th',class_='first',text=[current_month]).find_parent('table')
+	table_next_month = soup.find('th',class_='first',text=[next_month]).find_parent('table')
+	tables = [table_month,table_next_month]
+	#scrap the data from the tables
+	for table in tables:
+		month = table.find('th',class_='first').string
+		print month
+		for row in table.find_all('tr'):
+			home = False
+			channel = None
+			if row.find('td',class_="time") and row.find('td',class_="competition"):
+				if row.find('td',class_='home-away').string == 'H':
+					home = True
+				if row.find('td',class_="tv-channel"):
+					channel = row.find('td',class_="tv-channel").string.strip()
+				opponenent = row.find('td',class_="opposition").string.strip()
+				time = row.find('td',class_="time").string.strip()
+				if len(time) == 0:
+					time = "15:00"
+				day = row.find('td',class_="first").string
+				if day.find('/'):
+					day = day.split('/')[0]
+				if month == 'December':
+					year = '2012'
+				else:
+					year = '2013'
+				competition = row.find('td',class_="competition").string
+				date = day+ " " +month+ " " + year + " " + time
+				date = datetime.strptime(date,'%d %B %Y %H:%M')
+				date = uktz.localize(date)
+				channel_us = None
+				fixture = Fixture(opponenent, home, channel,channel_us, date, competition)
+				db.session.add(fixture)
+				db.session.commit()
+
+scrap_fixture_time()
+get_us_data()
+
+
+
