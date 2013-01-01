e6f0eb7450220506c22ac5806bd831cd20ab988a
Sylvain Giuliani <sgiuliani@gmail.com>
2013-01-01 15:00:54 +0000

    fix layout bug

--- helpers.py
+++ helpers.py
@@ -81,12 +81,22 @@ def get_stream():
 	url = "http://atdhe.eu/soccer"
 	html = requests.get(url).text
 	soup = BeautifulSoup(html,'lxml')
+	deleted = 0
+	added = 0
+	for stream in Stream.query.all():
+		db.session.delete(stream)
+		db.session.commit()
+		print "deleting..."
+		deleted += 1
 
 	streams = soup.find_all(text=re.compile('Arsenal'))
 	for s in streams:
 		link = Stream(s.parent['href'])
 		db.session.add(link)
 		db.session.commit()
+		print "adding..."
+		added += 1
+	print "Finished getting new stream. Deleted %s and added %s streams"%(deleted,added)
 
 scrap_fixture_time()
 get_us_data()
